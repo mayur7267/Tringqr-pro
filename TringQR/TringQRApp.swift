a298ebdd97f94abdb8800fcb8a510aeb033422f3
mayur7267 <mayurlate.7267@gmail.com>
2024-11-30 01:23:21 +0530

    Initial Commit

---
+++ TringQR/TringQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TringQRApp.swift
+//  TringQR
+//
+//  Created by Mayur on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TringQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TringQRTests/TringQRTests.swift
@@ -0,0 +1,17 @@
+//
+//  TringQRTests.swift
+//  TringQRTests
+//
+//  Created by Mayur on 30/11/24.
+//
+
+import Testing
+@testable import TringQR
+
+struct TringQRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
