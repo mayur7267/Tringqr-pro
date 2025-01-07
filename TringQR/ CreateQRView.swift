30aa67b1613485186cdd7aad20e4c3cefd0d0df9
mayur7267 <mayurlate.7267@gmail.com>
2025-01-07 22:28:30 +0530

    updated code

---
+++ TringQR/ CreateQRView.swift
@@ -0,0 +1,18 @@
+//
+//   CreateQRView.swift
+//  TringQR
+//
+//  Created by Mayur on 07/01/25.
+//
+
+import SwiftUI
+
+struct _CreateQRView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _CreateQRView()
+}
